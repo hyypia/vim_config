c8f26cbe1e8ae5b94dee8afb5b967fc15d513ed3
igor <igortereschuk92@gmail.com>
2025-02-24 18:51:47 +0100

    used coc-extension

--- init.lua
+++ init.lua
@@ -5,7 +5,7 @@ set.mouse = 'a'
 set.encoding = 'utf-8'
  -- set.noswapfile = true
 set.tabstop = 4
-set.softtabstop = 4
+-- set.softtabstop = 4
 set.shiftwidth = 4
 set.expandtab = true
 set.autoindent = true
@@ -16,65 +16,73 @@ set.updatetime = 300
 set.cmdheight = 2           -- more space in the neovim command line for displaying messages
 set.signcolumn = "yes"      -- always show the sign column, otherwise it would shift the text each time
 
+
+-- Keymaps
+local keyset = vim.keymap.set
 -- jk to escape
-vim.keymap.set('i', 'jk', '<Esc>')
+keyset('i', 'jk', '<Esc>')
 
 -- ctrl+s saving
-vim.keymap.set("n", "<C-s>", ":w<CR>", { noremap = true, silent = true })
-vim.keymap.set("i", "<C-s>", "<Esc>:w<CR>i", { noremap = true, silent = true })
-vim.keymap.set("v", "<C-s>", "<Esc>:w<CR>gv", { noremap = true, silent = true })
+keyset("n", "<C-s>", ":w<CR>", { noremap = true, silent = true })
+keyset("i", "<C-s>", "<Esc>:w<CR>i", { noremap = true, silent = true })
+keyset("v", "<C-s>", "<Esc>:w<CR>gv", { noremap = true, silent = true })
 
--- plugins
+-- Plugins
 local Plug = vim.fn['plug#']
 vim.call('plug#begin')
 
-Plug 'Mofiqul/dracula.nvim'
--- Plug('catppuccin/nvim', {['as'] = 'catppuccin' })
-Plug 'williamboman/mason.nvim'
-Plug 'williamboman/mason-lspconfig.nvim'
-Plug 'neovim/nvim-lspconfig'
-Plug 'nvimtools/none-ls.nvim'
-Plug 'nvim-lua/plenary.nvim'
-Plug('nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'})
-Plug 'windwp/nvim-autopairs'
+-- Visual
+Plug('dracula/vim', { ['as'] = 'dracula' })
 Plug 'nvim-lualine/lualine.nvim'
 Plug 'nvim-tree/nvim-web-devicons'
 
-Plug('ms-jpq/coq_nvim', {['branch'] = 'coq'})
-Plug('ms-jpq/coq.artifacts', {['branch'] = 'artifacts'})
-Plug('ms-jpq/coq.thirdparty', {['branch'] = '3p'})
+Plug('nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'})
+Plug 'windwp/nvim-autopairs'
 
+-- Code autocomplete and snippets (:CocInstall coc-pyright /
+-- clangd / html / json / tsserver / sql-language-server)
+Plug('neoclide/coc.nvim', {['branch'] = 'release'})
+Plug('yaegassy/coc-ruff', {['do'] = 'yarn install --frozen-lockfile'})
+Plug('yaegassy/coc-black-formatter', {['do'] = 'yarn install --frozen-lockfile'})
+                                                        
 vim.call('plug#end')
-
-require('configs.theme')
 
 vim.cmd[[
     colorscheme dracula
     highlight ColorColumn ctermbg=60
     highlight LineNr ctermbg=140
+    highlight Normal guibg=none
+    highlight NonText guibg=none
+    highlight Normal ctermbg=none
+    highlight NonText ctermbg=none
 ]]
 
-
-require("configs.lsp")
 require('configs.lualine')
-
--- Use LSP for omnifunc for Python files
-vim.cmd([[autocmd FileType python setlocal omnifunc=v:lua.vim.lsp.omnifunc]])
-
-
--- python keybidings
-vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to Definition" })
-vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Find References" })
-vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover Documentation" })
-vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename Symbol" })
-vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Actions" })
-
 require("nvim-treesitter.configs").setup({
     ensure_installed = { "python" },  -- install Python parser
     highlight = { enable = true },    -- enable syntax highlighting
 })
-
 require('nvim-autopairs').setup({
     check_ts = true,
     disable_filetype = { "TelescopePrompt" , "vim" },
 })
+
+-- Coc Keymaps
+-- Use Tab for trigger completion with characters ahead and navigate
+-- NOTE: There's always a completion item selected by default, you may want to enable
+-- no select by setting `"suggest.noselect": true` in your configuration file
+-- NOTE: Use command ':verbose imap <tab>' to make sure Tab is not mapped by
+-- other plugins before putting this into your config
+local opts = {silent = true, noremap = true, expr = true, replace_keycodes = false}
+keyset("i", "<TAB>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', opts)
+keyset("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
+
+-- Make <CR> to accept selected completion item or notify coc.nvim to format
+-- <C-g>u breaks current undo, please make your own choice
+keyset("i", "<cr>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
+
+-- GoTo code navigation
+keyset("n", "gd", "<Plug>(coc-definition)", {silent = true})
+keyset("n", "gy", "<Plug>(coc-type-definition)", {silent = true})
+keyset("n", "gi", "<Plug>(coc-implementation)", {silent = true})
+keyset("n", "gr", "<Plug>(coc-references)", {silent = true})
